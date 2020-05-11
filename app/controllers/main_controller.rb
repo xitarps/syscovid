0327fad7eaa4acec8752ce721c361c403710460a
Fabiane Albuquerque <bii.nett@gmai.com>
2020-05-11 19:22:14 -0300

    Add homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def terms
   end
+
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'terms', to: 'main#terms'
+  
+  root 'main#index'
 end
