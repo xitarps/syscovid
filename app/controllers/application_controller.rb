e12b97e016a4c12b33b4cbf35ede9ba1e001a0a1
Fabiane Albuquerque <bii.nett@gmai.com>
2020-05-12 00:20:33 -0300

    Customize registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_zip_code, :email, :password,:password_confirmation, :country)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:user_zip_code, :email, :password,:password_confirmation, :current_password, :country)}
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  @country_ob = Country.new
+  @countries = @country_ob.list_countries
+
+  validates :user_zip_code, zipcode: { country_code_attribute: :country }, presence: true
+  validates :country, presence: true, inclusion: {in: @countries}
 end
