e441385c4610d718a9e3c51962f831dd0bd2d291
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-15 00:25:18 -0300

    Add login page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-first_user = FactoryBot.create(:user)
+def generate_password
+  letters = ('a'..'z').to_a
+  s_letters = ['@','_','!','#','&']
+  numbers = (0..9).to_a
+  moment = DateTime.now.to_s.gsub(':','-').split('-').shuffle
+  size = (6..10).to_a.shuffle.first
+  pwd = ''
+  size.times { pwd = pwd+letters.shuffle.last.upcase+s_letters.shuffle.last.upcase+numbers.shuffle.last.to_s+(moment.shuffle.pop unless moment.empty?)}
+  pwd
+end
+
+User.create(email:'aarondasilva@syscovid.com', password: generate_password, user_zip_code:'01001000', country:'BR', gender: 1, name: 'Aaron da silva',age: 1)
