3079fc80da890da02188cab7b9b4802596396b8b
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-18 04:02:51 -0300

    Fix social distance view test - expect css

--- spec/features/user_view_social_distances_spec.rb
+++ spec/features/user_view_social_distances_spec.rb
@@ -15,7 +15,8 @@ feature 'view social distances' do
       expect(page).to have_content(I18n.t(:bedrooms))
       expect(page).to have_content(I18n.t(:many_contacts).capitalize)
       expect(page).to have_content(I18n.t(:been_outdoor).capitalize)
-      expect(page).to have_css("input[value='#{I18n.t(:save).capitalize}']")
+      expect(page).to have_content(I18n.t(:save).capitalize)
+      # expect(page).to have_css("input[value='#{I18n.t(:save).capitalize}']")
     end
   end
 end
